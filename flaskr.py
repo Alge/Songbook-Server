b8cf26a7c976d36740fde7b2262307a4d043d239
Martin Alge <maralg@kth.se>
2014-11-06 17:34:01 +0100

    updated main file

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,6 @@
 # all the imports
 import sqlite3
+from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
@@ -19,3 +20,9 @@ def connect_db():
 
 if __name__ == '__main__':
     app.run()
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
